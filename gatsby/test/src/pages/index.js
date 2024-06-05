e4347993480e290dcb64f9e15569fe11e53aa451
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-06-05 09:55:21 -0300

    add initial code for gatsbyjs

---
+++ gatsby/test/src/pages/index.js
@@ -0,0 +1,37 @@
+import * as React from "react"
+import { Script } from "gatsby"
+
+const pageStyles = {
+  color: "#232129",
+  padding: "6rem",
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+}
+
+const widget = {
+  marginTop: "2rem",
+  maxWidth: "45rem",
+  width: "100%"
+}
+
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      <h1>Test Podmind</h1>
+
+      <Script async src="https://podmind.voxgig.com/widget/voxgig-podmind-ask.js" />
+      <div style={widget}>
+        <voxgig-podmind-ask
+          apikey="your-api-key"
+          debug="true">
+        </voxgig-podmind-ask>
+      </div>
+    </main >
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Test Podmind</title>
