eb399b376ebe2262eac856dac9c38d0566628b4f
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-06-08 16:00:38 -0300

    use lib

--- nextjs/test/src/app/page.js
+++ nextjs/test/src/app/page.js
@@ -1,18 +1,13 @@
+'use client'
 import styles from "./page.module.css"
-import Script from "next/script"
+import PodW from '@voxgig/podw-next'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Test Podmind</h1>
-
-      <Script src="https://podmind.voxgig.com/widget/voxgig-podmind-ask.js" />
-      <div className={styles.widget}>
-        <voxgig-podmind-ask
-          apikey="your-api-key"
-          debug="true">
-        </voxgig-podmind-ask>
-      </div>
-    </main>
+      <PodW
+        apikey="your-api-key"
+        debug="true" />
+    </main >
   );
 }
