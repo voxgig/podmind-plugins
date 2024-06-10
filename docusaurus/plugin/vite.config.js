fcaa3fb7654c0850374b977ae4992da9cc0e606d
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-06-10 07:47:06 -0300

    update vite config

--- docusaurus/plugin/vite.config.js
+++ docusaurus/plugin/vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
       target: 'es6',
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', '@docusaurus/core'],
       output: {
         globals: {
           react: 'React',
