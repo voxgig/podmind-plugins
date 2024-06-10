a45d453297040bec231f16cf1670237edb0f1d5a
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-06-10 05:27:43 -0300

    update plugin

---
+++ docusaurus/plugin/src/PodW.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Head from '@docusaurus/Head';
+
+export default function PodW({ Head, apikey, debug }) {
+  return (
+    <>
+      <Head>
+        <script async src="https://podmind.voxgig.com/widget/voxgig-podmind-ask.js"></script>
+      </Head>
+      <voxgig-podmind-ask apikey={apikey} debug={debug ? "true" : "false"} ></voxgig-podmind-ask>
+    </>
+  );
+}
+
