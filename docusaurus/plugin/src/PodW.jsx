55f565de6a689e111f9e5721958d182f4e0644e4
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-06-10 07:58:48 -0300

    update plugin code

--- docusaurus/plugin/src/PodW.jsx
+++ docusaurus/plugin/src/PodW.jsx
@@ -4,11 +4,14 @@ export default function PodW({ Head, apikey, debug }) {
   return (
     <>
       <Head>
-        <script async src="https://podmind.voxgig.com/widget/voxgig-podmind-ask.js">
+        <script
+          async
+          src="https://podmind.voxgig.com/widget/voxgig-podmind-ask.js">
         </script>
       </Head>
 
-      <voxgig-podmind-ask apikey={apikey}
+      <voxgig-podmind-ask
+        apikey={apikey}
         debug={debug ? "true" : "false"} >
       </voxgig-podmind-ask>
     </>
