12785b1de18e3d12688d1a9ddd559e1a83ecd449
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-06-10 05:30:55 -0300

    update format

--- docusaurus/plugin/src/PodW.jsx
+++ docusaurus/plugin/src/PodW.jsx
@@ -5,9 +5,13 @@ export default function PodW({ Head, apikey, debug }) {
   return (
     <>
       <Head>
-        <script async src="https://podmind.voxgig.com/widget/voxgig-podmind-ask.js"></script>
+        <script async src="https://podmind.voxgig.com/widget/voxgig-podmind-ask.js">
+        </script>
       </Head>
-      <voxgig-podmind-ask apikey={apikey} debug={debug ? "true" : "false"} ></voxgig-podmind-ask>
+
+      <voxgig-podmind-ask apikey={apikey}
+        debug={debug ? "true" : "false"} >
+      </voxgig-podmind-ask>
     </>
   );
 }
