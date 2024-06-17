2ed4a8759ba7b21ba32bd4098ece42d8cc4d1472
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-06-17 10:29:51 -0300

    add async to script

--- nuxt/test/pages/index.vue
+++ nuxt/test/pages/index.vue
@@ -1,11 +1,30 @@
 <template>
-  <h1>
-    Podmind Test
-  </h1>
+  <div>
+    <h1>
+      Podmind Test
+    </h1>
+
+    <voxgig-podmind-ask apikey="your-api-key" debug="true">
+    </voxgig-podmind-ask>
+  </div>
 </template>
+
+
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  head() {
+    return {
+      title: 'test',
+      script: [
+        {
+          hid: 'podmind',
+          async: true,
+          src: 'https://podmind.voxgig.com/widget/voxgig-podmind-ask.js'
+        }
+      ]
+    }
+  },
 }
 </script>
