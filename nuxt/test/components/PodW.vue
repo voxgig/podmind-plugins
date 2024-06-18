e40652b8f18db1a954bb6b60f28b0ed025610ce0
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-06-18 16:49:05 -0300

    update component header for nuxt3

--- nuxt/test/components/PodW.vue
+++ nuxt/test/components/PodW.vue
@@ -2,3 +2,17 @@
   <voxgig-podmind-ask apikey="your-api-key" debug="true">
   </voxgig-podmind-ask>
 </template>
+
+<script lang="ts">
+export default defineNuxtComponent({
+  head: {
+    script: [
+      {
+        async: true,
+        hid: 'podw-script',
+        src: 'https://podmind.voxgig.com/widget/voxgig-podmind-ask.js'
+      }
+    ],
+  }
+}) 
+</script>
--- nuxt/test/nuxt.config.ts
+++ nuxt/test/nuxt.config.ts
@@ -1,15 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  app: {
-    head: {
-      script: [
-        {
-          async: true,
-          hid: 'podw-script',
-          src: 'https://podmind.voxgig.com/widget/voxgig-podmind-ask.js'
-        }
-      ],
-    }
-  }
 })
