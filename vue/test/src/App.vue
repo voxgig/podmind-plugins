d69e6f90139d967ef230c07e23777fee2a5de01a
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-06-19 08:38:37 -0300

    add init test code for vue

---
+++ vue/test/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useScriptTag } from '@vueuse/core'
+
+useScriptTag('https://podmind.voxgig.com/widget/voxgig-podmind-ask.js')
+</script>
+
+<template>
+  <div class="main">
+    <h1>Podmind Test</h1>
+
+    <voxgig-podmind-ask apikey="your-api-key" debug="true">
+    </voxgig-podmind-ask>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
