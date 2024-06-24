927730112f8933e04ab9b73117757fb6b493e826
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-06-24 09:11:26 -0300

    export embed logic to component

--- react/test/src/App.js
+++ react/test/src/App.js
@@ -1,23 +1,14 @@
-import { useEffect } from 'react'
-
 import './App.css';
+import PodW from './components/PodW';
 
 function App() {
-  useEffect(() => {
-    const script = document.createElement('script')
-    script.src = "https://podmind.voxgig.com/widget/voxgig-podmind-ask.js"
-    return () => document.head.appendChild(script)
-  }, [])
-
   return (
     <>
       <h1>Podmind Test</h1>
 
-      <voxgig-podmind-ask
+      <PodW
         apikey="your-api-key"
-        debug="true"
-      >
-      </voxgig-podmind-ask>
+      />
     </>
   )
 }
