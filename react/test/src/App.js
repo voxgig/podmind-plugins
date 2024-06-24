84845713cfaaee6bc04cbb92ad0ea3b16829e209
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-06-24 09:21:31 -0300

    remove unused files

--- react/test/src/App.js
+++ react/test/src/App.js
@@ -3,13 +3,13 @@ import PodW from './components/PodW';
 
 function App() {
   return (
-    <>
+    <div className='main'>
       <h1>Podmind Test</h1>
 
       <PodW
         apikey="your-api-key"
       />
-    </>
+    </div>
   )
 }
 
