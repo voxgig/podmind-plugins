57a6cbad393214d453f9f8cdf96b1931912abcff
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-06-25 09:12:34 -0300

    render widget by adding script to all pages

--- angular/test/src/app/app.component.ts
+++ angular/test/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,7 +6,10 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppComponent {
   title = 'test';
