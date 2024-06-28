a0f50d6c873921a2c1415148ae390f18bc1ebbcb
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-06-28 04:51:02 -0300

    add author

--- wordpress/plugin/podmind-widget.php
+++ wordpress/plugin/podmind-widget.php
@@ -7,6 +7,7 @@
  * Requires PHP:      7.2
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Author:            Leonardo Cunha
  * Text Domain:       podmind-widget-plugin
  */
 
