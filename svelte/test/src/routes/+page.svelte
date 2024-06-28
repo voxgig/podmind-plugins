f13de5d166f58c4109cd833ed54254866b462bb1
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-06-28 05:05:57 -0300

    add init svelte test code

---
+++ svelte/test/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<svelte:head>
+  <script async src="https://podmind.voxgig.com/widget/voxgig-podmind-ask.js">
+  </script>
+</svelte:head>
+
+<section>
+  <h1>Podmind Test</h1>
+
+  <voxgig-podmind-ask apikey="your-api-key" debug="true"> </voxgig-podmind-ask>
+</section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
