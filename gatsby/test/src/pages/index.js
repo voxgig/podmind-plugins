6fec563ebe27a767bed2cabe24b4321b38929d3f
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-07-03 17:36:06 -0300

    add plugin to test

--- gatsby/test/src/pages/index.js
+++ gatsby/test/src/pages/index.js
@@ -1,11 +1,7 @@
 import * as React from "react"
-// import { Script } from "gatsby"
 import PodW from '@voxgig/podw-gatsby'
 
 const pageStyles = {
-  color: "#232129",
-  padding: "6rem",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
