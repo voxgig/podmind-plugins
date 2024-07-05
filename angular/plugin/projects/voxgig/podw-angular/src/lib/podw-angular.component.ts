107c96c11401bbb77102a9167860cd4f4b4a440e
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-07-05 19:53:13 -0300

    update component name

--- angular/plugin/projects/voxgig/podw-angular/src/lib/podw-angular.component.ts
+++ angular/plugin/projects/voxgig/podw-angular/src/lib/podw-angular.component.ts
@@ -3,25 +3,25 @@ import { CUSTOM_ELEMENTS_SCHEMA, Component, Inject, Input } from '@angular/core'
 import { PodWAttr } from './pod-wattr';
 
 @Component({
-  selector: 'lib-podw-angular',
+  selector: 'lib-podw',
   standalone: true,
   imports: [],
   template: `
    <voxgig-podmind-ask
-   [attr.apikey]="podw.apikey"
-   [attr.debug]="podw.debug">
+   [attr.apikey]="podWAttr.apikey"
+   [attr.debug]="podWAttr.debug">
    </voxgig-podmind-ask>
   `,
   styles: ``,
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 
-export class PodwAngularComponent {
+export class PodWComponent {
   constructor(
     @Inject(DOCUMENT) private document: Document
   ) { }
 
-  @Input() podw!: PodWAttr
+  @Input() podWAttr!: PodWAttr
 
   ngOnInit() {
     const script = this.document.createElement('script')
